165b3e3d602b1766735a73cb2d5dd0f829d0321a
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-13 14:10:03 +0100

    Impostato Esercizio

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*
+
+    L'utente clicca su un bottone che genera una griglia di gioco quadrato:
+
+    -- 1) Ogni cella ha un numero progressivo da 1 a 100;
+    -- 2) Saranno 10 righe da 10 caselle;
+    -- 3) Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emette un messaggio in console con il numero della cella cliccata.
+
+*/
