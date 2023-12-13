921701d7365d2c582fd1f3f59955ba71304c8ac8
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-13 14:19:55 +0100

    Create Grid Container w/cell

--- js/main.js
+++ js/main.js
@@ -7,3 +7,16 @@
     -- 3) Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emette un messaggio in console con il numero della cella cliccata.
 
 */
+
+// Definisco una Variabile con la quale prendo il Container dall'HTML
+const containerGrid = document.querySelector('.container-grid');
+console.log('containerGrid', containerGrid, typeof containerGrid);
+
+// Definisco una Variabile con la quale creo la Cella da inserire nel Container
+const cell = document.createElement('div');
+console.log('cell', cell, typeof cell);
+
+cell.classList.add('cell');
+
+// "Appendo" la Cella al Contenitore 
+containerGrid.append(cell);
